e19ed4103db10585f913418ad6e50929697992f4
PC2410\ITS <ledanghanh@g.densan-ginza.co.jp>
2024-01-31 17:37:11 +0900

    done

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -75,10 +75,24 @@ WSGI_APPLICATION = 'PythonWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {  
+#     'default': {  
+#         'ENGINE': 'django.db.backends.mysql',  
+#         'NAME': 'hanh_db',  
+#         'USER': 'root',  
+#         'PASSWORD': '',  
+#         'HOST': 'localhost',  
+#     }  
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
